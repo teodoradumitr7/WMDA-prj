b61398cbebdf538657c959033c26f278bd40f9d9
teodoradumitr7 <teodoradt19@gmail.com>
2025-06-05 14:37:09 +0300

    added examples

--- main.py
+++ main.py
@@ -13,14 +13,8 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 # 1. Clean and encode
 df = clean_data(DATA_PATH)
-
-# from src.web_scraper import scrape_autovit
-# scraped_path = scrape_autovit(num_pages=2)  # sau câte pagini vrei
-# df = clean_data(scraped_path)
 df, encoders = encode_data(df, CATEGORICAL_VARS)
-
 print(df)
-
 
 # 2. Recommender
 features = df[NUMERIC_VARS + CATEGORICAL_VARS]
@@ -48,7 +42,6 @@ pipe.fit(X, y)
 print(f"\nRegresie Liniară R²: {pipe.score(X, y):.2f}")
 
 # 5. NN training
-# X_nn = torch.tensor(scaler.transform(df[NUMERIC_VARS]), dtype=torch.float32)
 X_nn = torch.tensor(X_scaled, dtype=torch.float32)
 y_nn = torch.tensor(y.values, dtype=torch.float32).view(-1, 1)
 X_train, X_test, y_train, y_test = train_test_split(X_nn, y_nn, test_size=0.2)
@@ -69,3 +62,60 @@ print("\n--- Confusion Matrix ---")
 print(confusion_matrix(y_test_cls, y_pred_cls))
 
 
+'''
+                         name  year    price
+42  FORD TRANSIT CUSTOM 2.0 D  2022  16500.0
+49            VOLVO S90 2.0 D  2019  20900.0
+19            VOLVO V60 2.0 L  2020  17990.0
+34          BMW SERIA 5 2.0 D  2020  26400.0
+63               BMW X3 2.0 D  2018  24900.0
+
+intrari:
+Introduceți detaliile mașinii:
+Anul (ex: 2018): 2017
+Km rulați: 25000
+Tip combustibil (Diesel/Benzina): Benzina
+
+Se remarca cele mai apropiate 5 mașini conform preferintelor introduse si conform algoritmului de recomandare K-Nearest Neighbors, cu k=5.
+Modelul a comparat această intrare cu toate din baza de date, folosind caracteristicile (year, km_driven, fuel) și a găsit cele mai asemănătoare anunțuri.
+
+Regresie Liniară R²: 0.37
+R² = coeficientul de determinare ce măsoară cât de bine se pot explica valorile prețurilor reale folosind modelul
+    doar 37% din variația prețurilor este explicată de model.
+    modelul de regresie liniară nu este foarte bun, valoare mai aproape de 0 decat de 1
+    poate fi îmbunătățit cu mai multe variabile (ex: marca, tip caroserie etc.).
+
+    
+
+--- Classification Report ---
+              precision    recall  f1-score   support
+
+       Buget       0.40      1.00      0.57         2
+       Mediu       0.75      0.50      0.60         6
+     Premium       0.80      0.67      0.73         6
+
+    accuracy                           0.64        14
+   macro avg       0.65      0.72      0.63        14
+weighted avg       0.72      0.64      0.65        14
+
+Precision: cât de precise sunt predicțiile (ex: din toate mașinile prezise ca "Premium", câte chiar erau "Premium"?).
+Recall: din toate mașinile "Premium" reale, câte au fost recunoscute corect?
+f1-score: media armonică dintre precision și recall (echilibru).
+support: câte exemple au fost în fiecare clasă.
+Acuratete 64%.
+
+Clasa "Buget" are doar 2 exemple, dar a fost prezisă 100% corect (recall = 1.00), dar modelul e nesigur (precision = 0.40).
+Clasa "Mediu" și "Premium" sunt mai echilibrate, dar încă au erori.
+
+--- Confusion Matrix ---
+[[2 0 0]    - Buget
+ [2 3 1]    - Mediu
+ [1 1 4]]   - Premium
+
+
+ 2+3+1=6 exemple reale "Mediu":
+    2 prezise greșit ca "Buget"
+    3 corect ca "Mediu"
+    1 greșit ca "Premium"
+
+'''
